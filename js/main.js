21fbbb92b3bc2c6fc2318e468d30c7b414e25bd8
ualiu <urim.aliu@gmail.com>
2022-08-01 21:59:57 -0400

    second commit

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function getFetch(){
       .then(res => res.json()) 
       .then(data => {
         console.log(data)
+
         const currentTemp = data.main.temp
         const currentFeel = data.main.feels_like
         const currentDesc = data.weather[0].description
@@ -19,11 +20,20 @@ function getFetch(){
 
         document.getElementById('city').innerText = `Weather in ${choice}`
         document.getElementById('temp').innerText = `${currentTemp}°C`
-        
-       document.getElementById('descritiption').innerText = `Conditions: ${currentDesc}`
-       document.getElementById('feels-like').innerText = `Feels like: ${currentFeel}°C`
-       document.getElementById('humidity').innerText = `Humidity: ${currentHumid}%`
-       document.getElementById('wind').innerText = `Wind-speed: ${currentWind}km/h`
+        document.getElementById('descritiption').innerText = `Conditions: ${currentDesc}`
+        document.getElementById('feels-like').innerText = `Feels like: ${currentFeel}°C`
+        document.getElementById('humidity').innerText = `Humidity: ${currentHumid}%`
+        document.getElementById('wind').innerText = `Wind-speed: ${currentWind}km/h`
+
+        if (currentTemp >= 0 && currentTemp <= 10 ) {
+          document.getElementById('message-to-user').innerText = `It will be a cool day in ${choice} today. Bring a jacket!`
+        } else if (currentTemp >= 11 && currentTemp <= 20) {
+          document.getElementById('message-to-user').innerText = `It will be a pleasant day in ${choice} today!`
+        } else if ( currentTemp >= 21) {
+          document.getElementById('message-to-user').innerText = `It will be a warm day in ${choice} today. Drink lots of water!`
+        } else {
+          document.getElementById('message-to-user').innerText = `It will be a cold day in ${choice} today. Make sure you dress up warm!`
+        }
        
       })
       .catch(err => {
